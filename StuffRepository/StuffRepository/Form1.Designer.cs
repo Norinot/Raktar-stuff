d5d944b78661881045a1135f7cc11820f3d6213e
Norinot <naruto010190@gmail.com>
2021-04-29 22:07:09 +0200

    asd

--- StuffRepository/StuffRepository/Form1.Designer.cs
+++ StuffRepository/StuffRepository/Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace StuffRepository
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            this.dataBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Location = new System.Drawing.Point(306, 174);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(60, 15);
+            this.username_label.TabIndex = 0;
+            this.username_label.Text = "Username";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(306, 212);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(57, 15);
+            this.password_label.TabIndex = 1;
+            this.password_label.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(375, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(375, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(353, 253);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(75, 23);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // dataBox
+            // 
+            this.dataBox.Location = new System.Drawing.Point(27, 92);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(206, 246);
+            this.dataBox.TabIndex = 5;
+            this.dataBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(353, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(486, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(609, 92);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataBox);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.username_label);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.RichTextBox dataBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- StuffRepository/StuffRepository/Form1.cs
+++ StuffRepository/StuffRepository/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace StuffRepository
 {
@@ -14,8 +15,76 @@ namespace StuffRepository
     {
         public Form1()
         {
+            
             InitializeComponent();
+            dataBox.Visible = false;
+            button1.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            StreamReader userread = new StreamReader("users.txt", Encoding.UTF8);
+            bool siker = false;
+            userread.ReadLine();
+            while (!userread.EndOfStream)
+            {
+                string[] split = userread.ReadLine().Split(';');
+                if (textBox1.Text == split[0]&&textBox2.Text== split[1])
+                {
+                    siker = true;
+                }
+                else
+                {
+                    MessageBox.Show("Helytelen jelszó!");
+                }
+            }
+            userread.Close();
+            if (siker)
+            {
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                textBox5.Visible = true;
+                dataBox.Visible = true;
+                button1.Visible = true;
+                username_label.Visible = false;
+                password_label.Visible = false;
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                login_button.Visible = false;
+            }
+            Read();
+            for (int i = 0; i < asd.Count; i++)
+            {
+                dataBox.Text += asd[i]+"\n";
+            }
+            
+        }
+        public static List<string> asd = new List<string>();
+        private void Read()
+        {
+            asd.Clear();
+            StreamReader readBooks = new StreamReader("konyvek.txt", Encoding.UTF8);
+            while (!readBooks.EndOfStream)
+            {
+                // string[] splitting = readBooks.ReadLine().Split(';');
+                asd.Add(readBooks.ReadLine());
+            }
+            readBooks.Close();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter wrtIn = new StreamWriter("konyvek.txt", true, Encoding.UTF8);
+            wrtIn.WriteLine(textBox3.Text + ";" + textBox4.Text + ";" + textBox5.Text);
+            wrtIn.Close();
+        }
     }
 }
